78f6a9c98c9dca976c7c45dfd32f85946a7a136f
Shiv Sharma <shivsharmavictory@gmail.com>
2020-03-27 14:49:31 -0400

    templets property explained

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is the main page")
+    return render(request, 'accounts/dashboard.html')
 
 def product(request):
-    return HttpResponse("This is the product page")
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse("This is the customer page")
+    return render(request, 'accounts/customer.html')
