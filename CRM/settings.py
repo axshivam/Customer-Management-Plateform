1f8e645c19b26bb0c82bec31bc3ffb8925756f12
Shiv Sharma <shivsharmavictory@gmail.com>
2020-04-04 10:48:08 -0400

    Password Reset Email

--- CRM/settings.py
+++ CRM/settings.py
@@ -129,3 +129,13 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+#SHTP CONFIGURATION
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '*********'
+EMAIL_HOST_PASSWORD = '*********'
